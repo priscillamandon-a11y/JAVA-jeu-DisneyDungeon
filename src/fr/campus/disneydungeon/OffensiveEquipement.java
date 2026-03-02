3d485e4665983e4e2e1fa92c1c31bd2c767ab94d
Priscilla Mandon <priscilla.mandon@le-campus-numerique.fr>
2026-03-02 12:03:49 +0100

    #1 1er commit - Mise en place

--- src/fr/campus/disneydungeon/OffensiveEquipement.java
+++ src/fr/campus/disneydungeon/OffensiveEquipement.java
@@ -1,5 +1,40 @@
 package fr.campus.disneydungeon;
 
 public class OffensiveEquipement {
+    //Variables d'instance (attributs)
+    private String type; // armes ou sortilege
+    private int attackLevel;
+    private String name;
+
+    // Constructeur
+    public OffensiveEquipement (String type, int attackLevel, String name){
+        // creation des attributs
+        this.type = type;
+        this.attackLevel = attackLevel;
+        this.name = name;
+    }
+    // Methodes :
+    // pour lire les valeurs -> GETTER
+    public String getType() {
+        return type;
+    }
+
+    public int getAttackLevel() {
+        return attackLevel;
+    }
+
+    public String getName() {
+        return name;
+    }
+    // Methodes :
+    // pour modifier les valeurs -> SETTER
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAttackLevel(int attackLevel) {
+        this.attackLevel = attackLevel;
+    }
+
 
 }
