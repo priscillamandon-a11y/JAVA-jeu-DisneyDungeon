b1dc0fc025b16ffa2670fc6c1f851c275d41e764
Priscilla Mandon <priscilla.mandon@le-campus-numerique.fr>
2026-03-09 15:05:05 +0100

    #17 Construction de Case vide + case ennemis

--- src/model/gameElements/cases/EnemyCell.java
+++ src/model/gameElements/cases/EnemyCell.java
@@ -1,10 +1,19 @@
 package model.gameElements.cases;
 
+import model.badGuys.Enemy;
+
 public class EnemyCell extends Cell {
+    // Variables
+    private Enemy enemy;
+
+    // constructeur :
+    public EnemyCell(enemy){
+        this.enemy = enemy;
+    }
 
     @Override
     public String toString() {
-        return "Case ennemis";
+        return enemy.toString();
     }
 
 
